4e1dba3746dba8ed21f7e633e454fe48dbe0d9a5
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-05-17 00:59:06 -0700

    Update sll.py

--- sll.py
+++ sll.py
@@ -10,6 +10,17 @@ class sll:
         newnode=node(val)
         newnode.nextt=self.head
         self.head=newnode
+    def insertatend(self):
+        val=int(input("enter val to insert at end"))
+        newnode=node(val)
+        if self.head is None:
+            self.head=newnode
+            return
+        temp=self.head
+        while(temp.nextt):
+           temp=temp.nextt
+        temp.nextt=newnode
+        
     def display(self):
         temp=self.head
         while temp!=None:
@@ -17,14 +28,17 @@ class sll:
             temp=temp.nextt
 s1=sll()
 ch=0
-while ch!=5:
-    print("1.insertatbeg 2.delatbeg 3.display 4.search 5.exit")
+while ch!=6:
+    print("1.insertatbeg 2.delatbeg 3.display 4.search 5.insertatend 6.exit")
     ch=int(input())
     if ch==1:
         s1.insertatbeg()
         s1.display()
     elif ch==3:
         s1.display()
+    elif ch==5:
+        s1.insertatend()
+        1.display()
     else:
         print("invalid choice")
         
